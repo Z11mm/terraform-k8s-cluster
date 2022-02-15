ff468080bef3b1ea566b6a090ef7500f150ab3cf
Ziimm <zimuzo.obiechina@deimos.co.za>
2022-02-15 11:13:56 +0100

    Switch state backend to gcs

--- dev/provider.tf
+++ dev/provider.tf
@@ -24,7 +24,7 @@ terraform {
     }
   }
 
-  backend "http" {
+  backend "gcs" {
     # address = "https://gitlab.com/api/v4/projects/32699910/terraform/state/default"
     # lock_address = "https://gitlab.com/api/v4/projects/32699910/terraform/state/default/lock"
     # unlock_address = "https://gitlab.com/api/v4/projects/32699910/terraform/state/default/lock"

b042ad48f71cdd7f29c4c706b6a40886db4879c9
Ziimm <zimuzo.obiechina@deimos.co.za>
2022-02-17 14:45:30 +0100

    fix:delete provider k8s

--- dev/provider.tf
+++ dev/provider.tf
@@ -55,11 +55,11 @@ provider "kubectl" {
 # ---------
 data "google_client_config" "default" {}
 
-provider "kubernetes" {
-  host                   = "https://${module.gke_cluster.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(module.gke_cluster.ca_certificate)
-}
+# provider "kubernetes" {
+#   host                   = "https://${module.gke_cluster.endpoint}"
+#   token                  = data.google_client_config.default.access_token
+#   cluster_ca_certificate = base64decode(module.gke_cluster.ca_certificate)
+# }
 
 provider "helm" {
   kubernetes {

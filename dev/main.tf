84e0bd13fb4984407461eede0a19e99a6d806134
Ziimm <zimuzo.obiechina@deimos.co.za>
2022-02-17 15:03:42 +0100

    fix:add subnet

--- dev/main.tf
+++ dev/main.tf
@@ -44,7 +44,7 @@ module "gke_cluster" {
   name              = "${var.cluster_name}-${var.environment}"
   region            = var.region
   network           = var.network_name
-  subnetwork        = element(module.vpc_network.subnets, 0)
+  subnetwork        = lookup(module.vpc_network.subnets)
   ip_range_pods     = var.ip_range_pods
   ip_range_services = var.ip_range_services
 }
